f3d5b516a9946eeecee25bedebcd136cfb1bbdc0
Aymeric-210297 <114666523+Aymeric-210297@users.noreply.github.com>
2022-12-01 13:29:32 +0100

    Ajout de l'HTML pour contact.php

--- revisions/php_html_css/contact.php
+++ revisions/php_html_css/contact.php
@@ -7,6 +7,33 @@
     <title>Formulaire de contact</title>
 </head>
 <body>
-    
+    <main>
+        <form action="">
+            <fieldset>
+                <legend>Formulaire de contact</legend>
+                <div>
+                    <label for="nom">Nom</label>
+                    <input type="text" name="nom" id="nom">
+                </div>
+                <div>
+                    <label for="prenom">Prénom</label>
+                    <input type="text" name="prenom" id="prenom">
+                </div>
+                <div>
+                    <label for="email">Email</label>
+                    <input type="email" name="email" id="email">
+                </div>
+                <div>
+                    <label for="telephone">Numéro de téléphone</label>
+                    <input type="tel" name="telephone" id="telephone">
+                </div>
+                <div>
+                    <label for="message">Votre message</label>
+                    <textarea name="message" id="message" cols="30" rows="10"></textarea>
+                </div>
+            </fieldset>
+            <button type="submit">Soumettre</button>
+        </form>
+    </main>
 </body>
 </html>
