38e27621065e93b883a7a9b14fbbc20ee0adad8e
Aymeric-210297 <114666523+Aymeric-210297@users.noreply.github.com>
2022-12-01 16:11:21 +0100

    Changement dans la structure des dossiers

--- revisions/php_html_css/index.php
+++ revisions/php_html_css/index.php
@@ -38,7 +38,7 @@ $tableau = [1, 8, 10, 12, 48, 78, 113, 114];
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/styles/index.css">
+    <link rel="stylesheet" href="styles/index.css">
     <title>Accueil</title>
 </head>
 <body>
