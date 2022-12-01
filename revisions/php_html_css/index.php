dbf9b7b592c9ef1097ab7cdf6f18e7c1ff394d97
Aymeric-210297 <114666523+Aymeric-210297@users.noreply.github.com>
2022-12-01 13:23:43 +0100

    Ajout de l'HTML pour index.php

--- revisions/php_html_css/index.php
+++ revisions/php_html_css/index.php
@@ -7,6 +7,26 @@
     <title>Accueil</title>
 </head>
 <body>
-    
+    <main>
+        <div>
+            <h2>Je veux des belles fonctions php (séparer analyse et affichage dans votre fichier)</h2>
+            <p>Voici une suite bien spéciale : TODO!</p>
+            <br />
+            <p>Le PGCD entre 21 et 15 vaut TODO!</p>
+        </div>
+        <div>
+            <h2>Montrer moi comment afficher proprement du code</h2>
+            <p>Dans la variable $tableau, voici les nombres pairs</p>
+            <ul>
+                <li>8</li>
+                <li>10</li>
+                <li>48</li>
+            </ul>
+            <p>Dans la variable $phrase "Bonjour a tous pour ces révisions". La dernière lettre de chaque mot est</p>
+            <ul>
+                <li>TODO!</li>
+            </ul>
+        </div>
+    </main>
 </body>
 </html>
