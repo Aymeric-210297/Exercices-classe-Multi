8fbbf5cea3ced8c69fb9b422cdd2f1c78c723281
Aymeric-210297 <114666523+Aymeric-210297@users.noreply.github.com>
2022-12-12 21:00:13 +0100

    Correction erreur dans le titre

--- index.php
+++ index.php
@@ -11,11 +11,11 @@ $files = scandir(__DIR__);
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Exercices class Multi</title>
+  <title>Exercices classe Multi</title>
 </head>
 
 <body>
-  <h1>Exercices class Multi</h1>
+  <h1>Exercices classe Multi</h1>
   <ul>
     <?php foreach ($files as $file) : ?>
       <?php if (is_dir($file) && $file != '.' && $file != '..' && $file != '.git' && $file != '.github') : ?>
